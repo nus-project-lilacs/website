235f1b48ec31f72ee03521bc109c5e5ea31342e7
Petrine Pang <petrinepang09@gmail.com>
2023-12-22 02:32:36 +0800

    Navigation & Routing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import NavBar from './components/NavBar/NavBar';
 import HomePage from './components/HomePage/HomePage';
