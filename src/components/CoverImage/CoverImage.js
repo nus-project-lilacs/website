cda6900156597ed10c1653017f1ce98dc0be3f59
hejin <hj827458@gmail.com>
2023-12-22 19:01:53 +0800

    Test npm CI/CD

--- src/components/CoverImage/CoverImage.js
+++ src/components/CoverImage/CoverImage.js
@@ -3,10 +3,8 @@ import React from "react";
 const CoverImage = () => {
   return (
     <div className="cover-image">
-      <div className="cover-text">
-        <h1>Project Lilacs</h1>
-        <h2>Helping the elderly in our community</h2>
-      </div>
+      <h1>Project Lilacs</h1>
+      <h2>Helping the elderly in our community</h2>
     </div>
   );
 };
