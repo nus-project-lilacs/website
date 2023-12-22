295c73292d5cae4d900af72b7ed7649a749e678e
hejin <hj827458@gmail.com>
2023-12-22 18:57:35 +0800

    Test npm ci and npm run build

---
+++ src/components/CoverImage/CoverImage.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const CoverImage = () => {
+  return (
+    <div className="cover-image">
+      <div className="cover-text">
+        <h1>Project Lilacs</h1>
+        <h2>Helping the elderly in our community</h2>
+      </div>
+    </div>
+  );
+};
