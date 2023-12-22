4659e941b95fd43364006624f8fb92b3017a30f4
hejin <hj827458@gmail.com>
2023-12-22 18:44:24 +0800

    Add pages folder

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import './App.css';
-import NavBar from './components/NavBar/NavBar';
-import HomePage from './components/HomePage/HomePage';
-import Sponsors from './components/Sponsors/Sponsors';
-import Resources from './components/Resources/Resources'
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "./App.css";
+import NavBar from "./components/NavBar/NavBar";
+import HomePage from "./pages/HomePage/HomePage";
+import Sponsors from "./pages/Sponsors/Sponsors";
+import Resources from "./pages/Resources/Resources";
 
 function App() {
   return (
     <Router>
       <div className="App">
         <header className="App-header">
-          <NavBar/>       
+          <NavBar />
         </header>
-            <Routes>
-              <Route path="/" exact element={<HomePage />} />
-              <Route path="/sponsors" exact element={<Sponsors />} />
-              <Route path="/resources" exact element={<Resources />} />
-            </Routes>
+        <Routes>
+          <Route path="/" exact element={<HomePage />} />
+          <Route path="/sponsors" exact element={<Sponsors />} />
+          <Route path="/resources" exact element={<Resources />} />
+        </Routes>
       </div>
     </Router>
   );
