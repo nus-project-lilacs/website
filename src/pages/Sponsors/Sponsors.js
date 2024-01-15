a235158915626f0d33b12fd8ad938566fdad9913
yespiggy <skylarye7@gmail.com>
2024-01-15 09:30:04 +0800

    update sponsor page

--- src/pages/Sponsors/Sponsors.js
+++ src/pages/Sponsors/Sponsors.js
@@ -1,7 +1,26 @@
+import React from 'react'
+import { SponsorList } from '../Sponsors/SponsorList'
+import SponsorItem from '../Sponsors/SponsorItem';
+import "../Sponsors/Sponsors.css";
 
+function Sponsors() {
+    return (
+        <div className='sponsors'>
+            <h1 className='sponsorTitle'>Sponsors</h1>
+            <div className="sponsorList">
+                {SponsorList.map((sponsorItem, key) => {
+                return (
+                    <SponsorItem 
+                      key={key} 
+                      image={sponsorItem.image} 
+                      name={sponsorItem.name} 
+                      info={sponsorItem.info} 
+                    />
+                );   
+            })}
+            </div>
+        </div>
+    );
+}
 
-const Sponsors = () => {
-    return 
-};
-
-export default Sponsors;
+export default Sponsors
